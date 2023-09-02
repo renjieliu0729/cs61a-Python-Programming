e954e595b0e7f9bb66d8ee409276c5b9c640ed28
renjiel0729 <renjiel@uchicago.edu>
2023-09-01 22:04:46 -0500

    update

---
+++ Notes:Scratch/learn.py
@@ -0,0 +1,21 @@
+def wears_jacket_with_if(temp, raining):
+    """
+    >>> wears_jacket_with_if(90, False)
+    False
+    >>> wears_jacket_with_if(40, False)
+    True
+    >>> wears_jacket_with_if(100, True)
+    True
+    """
+    "*** YOUR CODE HERE ***"
+
+    if temp < 60 or raining == True:
+        return True
+    else:
+        return False
+    
+
+print(wears_jacket_with_if(90, False))
+
+
+    
