674c27a82c834a4b42cb1d5f0e339e9b33bfe840
renjiel0729 <renjiel@uchicago.edu>
2023-09-03 11:56:20 -0500

    good

--- Notes:Scratch/learn.py
+++ Notes:Scratch/learn.py
@@ -18,4 +18,41 @@ def wears_jacket_with_if(temp, raining):
 print(wears_jacket_with_if(90, False))
 
 
-    
+
+def unique_digits(n):
+    """Return the number of unique digits in positive integer n.
+
+    >>> unique_digits(8675309) # All are unique
+    7
+    >>> unique_digits(1313131) # 1 and 3
+    2
+    >>> unique_digits(13173131) # 1, 3, and 7
+    3
+    >>> unique_digits(10000) # 0 and 1
+    2
+    >>> unique_digits(101) # 0 and 1
+    2
+    >>> unique_digits(10) # 0 and 1
+    2
+    """
+    "*** YOUR CODE HERE ***"
+    counter = 0
+    for i in str(n):
+        if has_digit(n,i):
+            counter = counter + 1
+            n = str(n).replace(i,"")
+    print(counter)
+
+def has_digit(n, k):
+    """Returns whether K is a digit in N.
+    >>> has_digit(10, 1)
+    True
+    >>> has_digit(12, 7)
+    False
+    """
+    "*** YOUR CODE HERE ***"
+    if str(k) in str(n):
+        return True
+    else:
+        return False
+
